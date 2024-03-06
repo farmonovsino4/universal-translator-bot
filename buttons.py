8f3a062c18fd63a64b78313ea4bc3dbf650d5780
farmonovsinno <farmonovsino2@gmail.com>
2024-03-06 14:28:06 +0500

    commit

---
+++ buttons.py
@@ -0,0 +1,5 @@
+from telebot.types import InlineKeyboardMarkup, InlineKeyboardButton
+
+voice_btn = InlineKeyboardMarkup().add(
+    InlineKeyboardButton("🎤 Voice", callback_data="voice")
+)
--- main.py
+++ main.py
@@ -5,6 +5,11 @@ from dotenv import load_dotenv
 from googletrans import Translator
 from deep_translator import GoogleTranslator
 import pytesseract
+from googletrans import Translator
+from gtts import gTTS
+import os
+
+translator = Translator()
 
 load_dotenv()
 
@@ -19,7 +24,30 @@ async def start(message: types.Message):
 
 @dp.message_handler()
 async def message(message: types.Message):
-    await message.answer(message.text)
+    lang = translator.detect(message.text).lang
+    if lang == 'en':
+        translated_uz = GoogleTranslator(source='en', target='uz').translate(message.text)
+        translated_ru = GoogleTranslator(source='en', target='ru').translate(message.text)
+        voice =gTTS(text=translated_uz, lang='ru').save('voice.mp3')
+        await message.answer(translated_uz)
+        await message.answer(translated_ru)
+        await message.answer_voice(open('voice.mp3', 'rb'))
+    elif lang == 'uz':
+        translated_en = GoogleTranslator(source='uz', target='en').translate(message.text)
+        translated_ru = GoogleTranslator(source='uz', target='ru').translate(message.text)
+        await message.answer(translated_en)
+        voice = gTTS(text=translated_en, lang='ru').save('voice.mp3')
+        await message.answer_voice(open('voice.mp3', 'rb'))
+        await message.answer(translated_ru)
+        voice = gTTS(text=translated_ru, lang='ru').save('voice.mp3')
+        await message.answer_voice(open('voice.mp3', 'rb'))
+    elif lang == 'ru':
+        translated_en = GoogleTranslator(source='ru', target='en').translate(message.text)
+        translated_uz = GoogleTranslator(source='ru', target='uz').translate(message.text)
+        await message.answer(translated_en)
+        voice = gTTS(text=translated_en, lang='ru').save('voice.mp3')
+        await message.answer_voice(open('voice.mp3', 'rb'))
+        await message.answer(translated_uz)
 
 
 if __name__ == '__main__':
